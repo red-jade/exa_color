c95def5a8f4e34927bf3cf35ee12911b5a25c071
red-jade <katagorikal@gmail.com>
2024-11-11 15:57:56 +0700

    Update build mechanism
    Change MIX_BUILD to EXA_BUILD
    
    Roll version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exa.Color.MixProject do
 
   @lib  :exa_color
   @name "Exa Color"
-  @ver  "0.3.0"
+  @ver  "0.3.1"
 
   # umbrella project
   @exa {:exa,
@@ -16,15 +16,21 @@ defmodule Exa.Color.MixProject do
   @mix_util Path.join(["deps", "exa", "mix_util.ex"])
 
   def project do
-    exa_deps =
-      if File.regular?(@mix_util) do
+    exa_deps = cond do
+       System.fetch_env("EXA_BUILD") in [:error, {:ok, "rel"}] -> 
+        # read auto-generated deps file
+        "deps.ex" |> Code.eval_file() |> elem(0) 
+
+      File.regular?(@mix_util) ->
+        # generate deps using exa umbrella project
         if not Code.loaded?(Exa.MixUtil) do
           [{Exa.MixUtil, _}] = Code.compile_file(@mix_util)
         end
+
         Exa.MixUtil.exa_deps(@lib, exa_libs())
-      else
-        # bootstrap
-        []
+      true ->
+        # bootstrap from exa umbrella project
+        [@exa]
       end
 
     [
